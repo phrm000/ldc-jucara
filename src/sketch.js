5dc3aa2866fe946fc5f0097d7fbe28160a722aca
Pedro Morais <61545580+phrm000@users.noreply.github.com>
2024-11-30 22:10:43 +0000

    Circles with hatch loop

--- src/sketch.js
+++ src/sketch.js
@@ -1,55 +1,96 @@
-
 //////////////////////////////////////////////////
 // P5 FUNCTIONS
 
 function preload() {
-    // If you are going to use custom image brush tips, include this in preload!
-    brush.preload();
+  // If you are going to use custom image brush tips, include this in preload!
+  brush.preload();
 }
 
-let palette = ["#002185", "#fcd300", "#ff2702", "#6b9404"]
-
-let x_values = []
-let y_values = []
-
-function setup () {
+let palette = ["#002185", "#fcd300", "#ff2702", "#6b9404"];
+let i = 0;
 
 
-    C.createCanvas()
-    background("#e2e7dc")
-    angleMode(RADIANS)
-    translate(-width/2,-height/2)
-        
-    
-    brush.noStroke();
-    brush.setHatch("HB", "#1E031D", 4);
-    brush.hatch(1, 90, {rand: 0, continuous: false, gradient: 0.01});
-    brush.bleed(0)
-
-    brush.circle(width/2,height/2, 50, false);
-    fill("#380533");
-    circle(width/2,height/2, 100, false);
+let points = [];
 
 
 
-    brush.fillTexture(1,1)
-    
+function setup() {
+  C.createCanvas();
+  background("#E9D9BD");
+  angleMode(RADIANS);
 
-    
-    
-    
+  translate(-width / 2, -height / 2);
+
+  //Draw first
+
+
+
+  
+
+
+
+
+   
+
 }
 
 function draw() {
 
+    brush.reDraw()
+
+translate(-width / 2, -height / 2);
 
 
+let circleR = 100;
+  let circleStartX = width / 2;
+  let circleStartY = circleR - height / 3; //Ver depois
+  let randomCircleFactor = random(0.5,1.4);
+
+  
+  let point1X = randomCircleFactor * circleStartX;
+  let point1Y = randomCircleFactor * circleStartY;
+  let point2X = randomCircleFactor * circleStartX + circleR;
+  let point2Y = randomCircleFactor * circleStartY + circleR;
+  let point3X = randomCircleFactor * circleStartX;
+  let point3Y = randomCircleFactor * circleStartY + circleR * 2;
+  let point4X = randomCircleFactor * circleStartX - circleR;
+  let point4Y = randomCircleFactor * circleStartY + circleR;
+
+
+
+  
+  while (i < 1) {
+    let randomColor = random(1, 100);
+    let colorR = map(randomColor, 1, 100, 168, 209);
+    let colorG = map(randomColor, 1, 100, 27, 69);
+    let colorB = map(randomColor, 1, 100, 26, 26);
+
+    brush.setHatch("charcoal", [colorR, colorG, colorB], random(1,2) * circleR/100);
+    brush.hatch(3, random(0, 270), {
+        rand: 0,
+        continuous: false,
+        gradient: 0.01 * i,
+      });
+
+    brush.fill([colorR,colorG,colorB], 100);
+    brush.fillTexture(0,0);
+    brush.bleed(0);
     
+    brush.noStroke();
+    brush.beginShape(1);
+    brush.vertex(point1X, point1Y);
+    brush.vertex(point2X, point2Y);
+    brush.vertex(point3X, point3Y);
+    brush.vertex(point4X, point4Y);
+    brush.endShape(CLOSE);
+
+
+
+
+   
+    i++;
+  }
+
+
 }
 
-function mouseDragged() {
-    loop()
-    frameRate(10)
-    x_values[0] = mouseX
-    y_values[0] = mouseY
-}
