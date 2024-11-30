a9d9bc479c57ccd08a6a7ede067b5f817ede7e82
Pedro Morais <61545580+phrm000@users.noreply.github.com>
2024-11-30 22:30:48 +0000

    Loop only with lines

--- src/sketch.js
+++ src/sketch.js
@@ -10,13 +10,10 @@ let palette = ["#002185", "#fcd300", "#ff2702", "#6b9404"];
 let i = 0;
 
 
-let points = [];
-
-
 
 function setup() {
   C.createCanvas();
-  background("#E9D9BD");
+  background("#0a0a0a");
   angleMode(RADIANS);
 
   translate(-width / 2, -height / 2);
@@ -24,6 +21,55 @@ function setup() {
   //Draw first
 
 
+
+
+  let circleR = 100;
+    let circleStartX = width / 2;
+    let circleStartY = circleR - height / 3; //Ver depois
+    let randomCircleFactor = random(0.5,1.4);
+  
+    
+    let point1X = randomCircleFactor * circleStartX;
+    let point1Y = randomCircleFactor * circleStartY;
+    let point2X = randomCircleFactor * circleStartX + circleR;
+    let point2Y = randomCircleFactor * circleStartY + circleR;
+    let point3X = randomCircleFactor * circleStartX;
+    let point3Y = randomCircleFactor * circleStartY + circleR * 2;
+    let point4X = randomCircleFactor * circleStartX - circleR;
+    let point4Y = randomCircleFactor * circleStartY + circleR;
+  
+  
+  
+    
+    while (i < 2) {
+      let randomColor = random(1, 100);
+      let colorR = map(randomColor, 1, 100, 168, 209);
+      let colorG = map(randomColor, 1, 100, 27, 69);
+      let colorB = map(randomColor, 1, 100, 26, 26);
+
+      console.log([colorR, colorG, colorB] )
+  
+      brush.setHatch("hatch_brush", [colorR, colorG, colorB], random(1,4) * circleR/100 * i/2);
+      brush.hatch(1, random(0, 0.3 * Math.PI), {
+          rand: 0,
+          continuous: false,
+          gradient: 0.2,
+        });
+  
+    // //   brush.fill([colorR,colorG,colorB], 100);
+    //   brush.fillTexture(1,0);
+    //   brush.bleed(0);
+      
+      brush.noStroke();
+      brush.beginShape(1);
+      brush.vertex(point1X, point1Y);
+      brush.vertex(point2X, point2Y);
+      brush.vertex(point3X, point3Y);
+      brush.vertex(point4X, point4Y);
+      brush.endShape(CLOSE);
+     
+      i++;
+    }
 
   
 
@@ -36,60 +82,9 @@ function setup() {
 
 function draw() {
 
-    brush.reDraw()
-
-translate(-width / 2, -height / 2);
-
-
-let circleR = 100;
-  let circleStartX = width / 2;
-  let circleStartY = circleR - height / 3; //Ver depois
-  let randomCircleFactor = random(0.5,1.4);
-
-  
-  let point1X = randomCircleFactor * circleStartX;
-  let point1Y = randomCircleFactor * circleStartY;
-  let point2X = randomCircleFactor * circleStartX + circleR;
-  let point2Y = randomCircleFactor * circleStartY + circleR;
-  let point3X = randomCircleFactor * circleStartX;
-  let point3Y = randomCircleFactor * circleStartY + circleR * 2;
-  let point4X = randomCircleFactor * circleStartX - circleR;
-  let point4Y = randomCircleFactor * circleStartY + circleR;
 
 
 
-  
-  while (i < 1) {
-    let randomColor = random(1, 100);
-    let colorR = map(randomColor, 1, 100, 168, 209);
-    let colorG = map(randomColor, 1, 100, 27, 69);
-    let colorB = map(randomColor, 1, 100, 26, 26);
-
-    brush.setHatch("charcoal", [colorR, colorG, colorB], random(1,2) * circleR/100);
-    brush.hatch(3, random(0, 270), {
-        rand: 0,
-        continuous: false,
-        gradient: 0.01 * i,
-      });
-
-    brush.fill([colorR,colorG,colorB], 100);
-    brush.fillTexture(0,0);
-    brush.bleed(0);
-    
-    brush.noStroke();
-    brush.beginShape(1);
-    brush.vertex(point1X, point1Y);
-    brush.vertex(point2X, point2Y);
-    brush.vertex(point3X, point3Y);
-    brush.vertex(point4X, point4Y);
-    brush.endShape(CLOSE);
-
-
-
-
-   
-    i++;
-  }
 
 
 }
