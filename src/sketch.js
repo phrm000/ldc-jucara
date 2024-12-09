cbdf0d7b2324942afe3f0bb5a0ebd793816add50
Pedro Morais <61545580+phrm000@users.noreply.github.com>
2024-12-09 14:37:08 +0000

    Drawing gralha

--- src/sketch.js
+++ src/sketch.js
@@ -32,7 +32,7 @@ function setup() {
   const tucanoX = width / 2;
   const tucanoY = height / 2;
 
-  drawTucano(tucanoRadius, tucanoX, tucanoY);
+  drawGralha(tucanoRadius, tucanoX, tucanoY);
 
 }
 
@@ -216,14 +216,19 @@ function drawBranch(points,fruitRadius){
 
 
 
-
-function drawTucano(tucanoR, tucanoStartX, tucanoStartY) {
+function drawGralha(tucanoR, tucanoStartX, tucanoStartY) {
   // Colorize
-  const tucanoDarkestFillColor = "#C3810D";
-  const tucanoLightestFillColor = "#DB7E1A";
+  const tucanoDarkestFillColor = "#0B2969";
+  const tucanoLightestFillColor = "#022B84";
 
   const tucanoDarkestFillColorRGB = hexToRgbCached(tucanoDarkestFillColor);
   const tucanoLightestFillColorRGB = hexToRgbCached(tucanoLightestFillColor);
+
+  const tucanoBeakDarkestFillColor = "#00010F";
+  const tucanoBeakLightestFillColor = "#06092F";
+
+  const tucanoBeakLightestFillColorRGB = hexToRgbCached(tucanoBeakLightestFillColor);
+  const tucanoBeakDarkestFillColorRGB = hexToRgbCached(tucanoBeakDarkestFillColor);
 
   // Randomize coordinates
   const tucanoCoordinates = randomCircleCoordinates(
@@ -261,33 +266,25 @@ function drawTucano(tucanoR, tucanoStartX, tucanoStartY) {
     drawCircleContainer(tucanoCoordinates);
 
 
-    const tucanoBeakDarkestFillColor = "#2A1B07";
-    const tucanoBeakLightestFillColor = "#332108";
-
-    const tucanoBeakLightestFillColorRGB = hexToRgbCached(tucanoBeakLightestFillColor);
-    const tucanoBeakDarkestFillColorRGB = hexToRgbCached(tucanoBeakDarkestFillColor);
+    
 
     let tucanoEyeDarkestFillColor;
     let tucanoEyeLightestFillColor;
 
 
 
-    for(let counterEyeFactor = 1; 3 >= counterEyeFactor; counterEyeFactor ++){
+    for(let counterEyeFactor = 2; 3 >= counterEyeFactor; counterEyeFactor ++){
       console.log(counterEyeFactor)
 
 
       switch(counterEyeFactor){
-        case 1:
-          tucanoEyeDarkestFillColor = "#720808";
-          tucanoEyeLightestFillColor = "#901010";
-          break;
         case 2:
           tucanoEyeDarkestFillColor = "#148DA6";
           tucanoEyeLightestFillColor = "#0B9BBA";
           break;
         case 3:
-          tucanoEyeDarkestFillColor = "#240101";
-          tucanoEyeLightestFillColor = "#724400";
+          tucanoEyeDarkestFillColor = "#000000";
+          tucanoEyeLightestFillColor = "#000000";
           break;
 
       }
@@ -295,8 +292,8 @@ function drawTucano(tucanoR, tucanoStartX, tucanoStartY) {
       let tucanoEyeLightestFillColorRGB = hexToRgbCached(tucanoEyeLightestFillColor);
       let tucanoEyeDarkestFillColorRGB = hexToRgbCached(tucanoEyeDarkestFillColor);
 
-      let tucanoEyesR = 3+ tucanoR/( 6 * counterEyeFactor);
-      let tucanoStartX = avgX ;
+      let tucanoEyesR = 3+ tucanoR/( 5 * counterEyeFactor);
+      let tucanoStartX = avgX - tucanoR/10 ;
       let tucanoStartY = avgY - tucanoR/2.5;
       setHatchTexture(tucanoEyesR, tucanoEyeLightestFillColorRGB, tucanoEyeDarkestFillColorRGB);
 
@@ -325,6 +322,151 @@ function drawTucano(tucanoR, tucanoStartX, tucanoStartY) {
     tucanoBeakLightestFillColorRGB,
     tucanoBeakDarkestFillColorRGB
   );
+
+
+  drawTucanBeak(
+    tucanoCoordinates,
+    tucanoR,
+    tucanoBeakLightestFillColorRGB,
+    tucanoBeakDarkestFillColorRGB
+  );
+
+
+
+ for(let counterFactor = 0; 3 > counterFactor; counterFactor++){
+  drawTucanoWings(
+    tucanoCoordinates,
+    tucanoR,
+    counterFactor
+  );
+
+  setHatchTexture(
+    tucanoR,
+    tucanoBeakLightestFillColorRGB,
+    tucanoBeakDarkestFillColorRGB
+  );
+
+  drawTucanoWings(
+    tucanoCoordinates,
+    tucanoR,
+    counterFactor
+  );
+}
+
+  
+}
+
+function drawTucano(tucanoR, tucanoStartX, tucanoStartY) {
+  // Colorize
+  const tucanoDarkestFillColor = "#C3810D";
+  const tucanoLightestFillColor = "#DB7E1A";
+
+  const tucanoDarkestFillColorRGB = hexToRgbCached(tucanoDarkestFillColor);
+  const tucanoLightestFillColorRGB = hexToRgbCached(tucanoLightestFillColor);
+
+  const tucanoBeakDarkestFillColor = "#00010F";
+  const tucanoBeakLightestFillColor = "#06092F";
+
+  const tucanoBeakLightestFillColorRGB = hexToRgbCached(tucanoBeakLightestFillColor);
+  const tucanoBeakDarkestFillColorRGB = hexToRgbCached(tucanoBeakDarkestFillColor);
+
+  // Randomize coordinates
+  const tucanoCoordinates = randomCircleCoordinates(
+    tucanoR,
+    tucanoStartX,
+    tucanoStartY
+  );
+
+  const avgX =
+    (tucanoCoordinates[0] +
+      tucanoCoordinates[2] +
+      tucanoCoordinates[4] +
+      tucanoCoordinates[6]) /
+    4;
+  const avgY =
+    (tucanoCoordinates[1] +
+      tucanoCoordinates[3] +
+      tucanoCoordinates[5] +
+      tucanoCoordinates[7]) /
+    4;
+    
+
+
+  setHatchFill(tucanoR, tucanoLightestFillColorRGB, tucanoDarkestFillColorRGB);
+
+  drawCircleContainer(tucanoCoordinates);
+
+
+
+    setHatchTexture(
+      tucanoR,
+      tucanoLightestFillColorRGB,
+      tucanoDarkestFillColorRGB
+    );
+    drawCircleContainer(tucanoCoordinates);
+
+
+    
+
+    let tucanoEyeDarkestFillColor;
+    let tucanoEyeLightestFillColor;
+
+
+
+    for(let counterEyeFactor = 1; 3 >= counterEyeFactor; counterEyeFactor ++){
+      console.log(counterEyeFactor)
+
+
+      switch(counterEyeFactor){
+        case 1:
+          tucanoEyeDarkestFillColor = "#720808";
+          tucanoEyeLightestFillColor = "#901010";
+          break;
+        case 2:
+          tucanoEyeDarkestFillColor = "#148DA6";
+          tucanoEyeLightestFillColor = "#0B9BBA";
+          break;
+        case 3:
+          tucanoEyeDarkestFillColor = "#000000";
+          tucanoEyeLightestFillColor = "#000000";
+          break;
+
+      }
+      console.log(tucanoEyeLightestFillColor)
+      let tucanoEyeLightestFillColorRGB = hexToRgbCached(tucanoEyeLightestFillColor);
+      let tucanoEyeDarkestFillColorRGB = hexToRgbCached(tucanoEyeDarkestFillColor);
+
+      let tucanoEyesR = 3+ tucanoR/( 5 * counterEyeFactor);
+      let tucanoStartX = avgX - tucanoR/10 ;
+      let tucanoStartY = avgY - tucanoR/2.5;
+      setHatchTexture(tucanoEyesR, tucanoEyeLightestFillColorRGB, tucanoEyeDarkestFillColorRGB);
+
+      brush.circle(tucanoStartX, tucanoStartY, tucanoEyesR, false)
+
+      
+      
+      
+    }
+
+
+
+
+  
+
+  setHatchFill(tucanoR, tucanoBeakLightestFillColorRGB, tucanoBeakDarkestFillColorRGB);
+  drawTucanBeak(
+    tucanoCoordinates,
+    tucanoR,
+    tucanoBeakLightestFillColorRGB,
+    tucanoBeakDarkestFillColorRGB
+  );
+
+  setHatchTexture(
+    tucanoR,
+    tucanoBeakLightestFillColorRGB,
+    tucanoBeakDarkestFillColorRGB
+  );
+
 
   drawTucanBeak(
     tucanoCoordinates,
@@ -369,10 +511,10 @@ function drawTucanBeak(tucanoCoordinates, tucanoR) {
     (tucanoCoordinates[1] +
       tucanoCoordinates[3] +
       tucanoCoordinates[5] +
-      tucanoCoordinates[7]) /
+      tucanoCoordinates[7]) / 
     4;
 
-  const centerX = avgX;
+  const centerX = avgX + tucanoR / 5;
   const centerY = avgY;
 
   brush.noStroke();
@@ -380,16 +522,20 @@ function drawTucanBeak(tucanoCoordinates, tucanoR) {
   brush.vertex(centerX, centerY);
 
   const startAngle = -PI / 3;
-  const endAngle = startAngle + (2 * PI) / 3;
-  const numPoints = 5;
+  const endAngle = startAngle + (2 * PI) / 2.75;
+  const numPoints = 20;
 
   const randomCircleFactor = () => random(0.95, 1.05);
-  console.log(randomCircleFactor)
+  const rotationAngle = - Math.PI / 6;
+
   for (let i = 0; i <= numPoints; i++) {
     const angle = lerp(startAngle, endAngle, i / numPoints);
-    const x = centerX + tucanoR * cos(angle) * 0.7 * randomCircleFactor();
-    const y = centerY + tucanoR * sin(angle) * 0.7 * randomCircleFactor();
-    brush.vertex(x, y);
+    const originalX = centerX + tucanoR * cos(angle) * 0.55 * randomCircleFactor();
+    const originalY = centerY + tucanoR * sin(angle) * 0.75 * randomCircleFactor();
+    const rotatedX = Math.cos(rotationAngle) * (originalX - centerX) - Math.sin(rotationAngle) * (originalY - centerY) + centerX;
+    const rotatedY = Math.sin(rotationAngle) * (originalX - centerX) + Math.cos(rotationAngle) * (originalY - centerY) + centerY;
+
+    brush.vertex(rotatedX, rotatedY);
   }
 
   brush.vertex(centerX, centerY);
@@ -399,9 +545,9 @@ function drawTucanBeak(tucanoCoordinates, tucanoR) {
 
 
 function drawTucanoWings(tucanoCoordinates, tucanoR, counterFactor) {
+  let mathFactor = counterFactor / 10;
+  console.log(mathFactor);
 
-  let mathFactor = counterFactor / 10
-  console.log(mathFactor)
   const avgX =
     (tucanoCoordinates[0] +
       tucanoCoordinates[2] +
@@ -415,22 +561,32 @@ function drawTucanoWings(tucanoCoordinates, tucanoR, counterFactor) {
       tucanoCoordinates[7]) /
     4;
 
-  const centerX = avgX - tucanoR * (0.3 - mathFactor) ;
-  const centerY = avgY + tucanoR*0.1;
+  const centerX = avgX + tucanoR * 0.7 - tucanoR * (0.9 - mathFactor);
+  const centerY = avgY + tucanoR * 0.1;
 
   brush.noStroke();
   brush.beginShape();
   brush.vertex(centerX, centerY);
 
-  const startAngle = -1.5 * PI + mathFactor * 2 ;
+  const startAngle = -1.5 * PI + mathFactor * 2;
   const endAngle = startAngle + mathFactor * 2 + (2 * PI) / 3;
   const numPoints = 3;
 
+  // Set the rotation angle in radians
+  const rotationAngle = -Math.PI / 6; // 30 degrees = π/6 radians
+
   for (let i = 0; i <= numPoints; i++) {
     const angle = lerp(startAngle, endAngle, i / numPoints);
-    const x = centerX + tucanoR * cos(angle) * 0.5;
-    const y = centerY + tucanoR * sin(angle) * 0.8;
-    brush.vertex(x, y);
+
+    // Original coordinates before rotation
+    const originalX = centerX + tucanoR * cos(angle) * 0.6;
+    const originalY = centerY + tucanoR * sin(angle) * 0.7;
+
+    // Apply 30-degree rotation
+    const rotatedX = Math.cos(rotationAngle) * (originalX - centerX) - Math.sin(rotationAngle) * (originalY - centerY) + centerX;
+    const rotatedY = Math.sin(rotationAngle) * (originalX - centerX) + Math.cos(rotationAngle) * (originalY - centerY) + centerY;
+
+    brush.vertex(rotatedX, rotatedY);
   }
 
   brush.vertex(centerX, centerY);
@@ -479,62 +635,61 @@ function drawTextContainer(productTitleFieldText,productDescFieldText){
 
 
 function draw() {
-  // translate(-width / 2, -height / 2);
+  translate(-width / 2, -height / 2);
 
-  // const fruitRadius = fruitRadiusSlider.value/400 * height;
-  // const startX = 0 - fruitRadius * 2 + width*0.05;
-  // const startY = 0 - fruitRadius;
+  const fruitRadius = fruitRadiusSlider.value/400 * height;
+  const startX = 0 - fruitRadius * 2 + width*0.05;
+  const startY = 0 - fruitRadius;
 
-  // const curveHeight = height;
-  // const curveWidth = width / 3;
-  // const angleOffset = 10 - angleOffsetSlider.value;
+  const curveHeight = height;
+  const curveWidth = width / 3;
+  const angleOffset = 10 - angleOffsetSlider.value;
 
-  // const blankSpaceChance = 10 - blankSpaceChanceSlider.value;
-  // const curveSpacing = map(curveSpacingSlider.value, 0, 20, 3, 0.3);
+  const blankSpaceChance = 10 - blankSpaceChanceSlider.value;
+  const curveSpacing = map(curveSpacingSlider.value, 0, 20, 3, 0.3);
 
-  // const fruitSpacing = fruitRadius * 1.2;
-  // const fruitsPerCurve = Math.floor(curveHeight / fruitSpacing);
-
-
-  // const productTitleFieldText = productTitleField.value
-  // const productDescFieldText = productDescriptionField.value
+  const fruitSpacing = fruitRadius * 1.2;
+  const fruitsPerCurve = Math.floor(curveHeight / fruitSpacing);
 
 
-  // if (random(1, 10) > blankSpaceChance) {
-  //   if (frameCount <= fruitsPerCurve) {
+  const productTitleFieldText = productTitleField.value
+  const productDescFieldText = productDescriptionField.value
+
+
+  if (random(1, 10) > blankSpaceChance) {
+    if (frameCount <= fruitsPerCurve) {
       
-  //     const t = (frameCount - 1) / (fruitsPerCurve - 1);
-  //     const x = startX + curveWidth * cos(t * PI + angleOffset);
-  //     const y = startY + curveHeight * t;
-  //     pointsForSpline.push([(x * random(0.95,1.05)),(y * random(0.95,1.05))]);
+      const t = (frameCount - 1) / (fruitsPerCurve - 1);
+      const x = startX + curveWidth * cos(t * PI + angleOffset);
+      const y = startY + curveHeight * t;
+      pointsForSpline.push([(x * random(0.95,1.05)),(y * random(0.95,1.05))]);
 
-  //     drawFruit(fruitRadius, x, y);
-  //   } else {
-  //     if(fruitsPerCurve == pointsForSpline.length){
-  //       drawBranch(pointsForSpline,fruitRadius);
+      drawFruit(fruitRadius, x, y);
+    } else {
+      if(fruitsPerCurve == pointsForSpline.length){
+        drawBranch(pointsForSpline,fruitRadius);
 
-  //       pointsForSpline = [];
+        pointsForSpline = [];
 
-  //       for(let j = 0; j < pointsForFruit.length; j++){
-  //         drawFruit(fruitRadius, pointsForFruit[j][0],pointsForFruit[j][1]);
-  //         //console.log(pointsForFruit[j].x)
-  //       }
-  //       pointsForFruit = [];
-  //     }
-  //     const curveIndex = Math.floor((frameCount - 1) / fruitsPerCurve);
-  //     const t = ((frameCount - 1) % fruitsPerCurve) / (fruitsPerCurve - 1);
-  //     const x =
-  //       startX + curveWidth * cos(t * PI + angleOffset) +
-  //       curveIndex * (curveWidth * curveSpacing);
-  //     const y = startY + curveHeight * t;
-  //     pointsForFruit.push([x,y]);
-  //     pointsForSpline.push([(x * random(0.95,1.05)),(y * random(0.95,1.05))]);
+        for(let j = 0; j < pointsForFruit.length; j++){
+          drawFruit(fruitRadius, pointsForFruit[j][0],pointsForFruit[j][1]);
+        }
+        pointsForFruit = [];
+      }
+      const curveIndex = Math.floor((frameCount - 1) / fruitsPerCurve);
+      const t = ((frameCount - 1) % fruitsPerCurve) / (fruitsPerCurve - 1);
+      const x =
+        startX + curveWidth * cos(t * PI + angleOffset) +
+        curveIndex * (curveWidth * curveSpacing);
+      const y = startY + curveHeight * t;
+      pointsForFruit.push([x,y]);
+      pointsForSpline.push([(x * random(0.95,1.05)),(y * random(0.95,1.05))]);
 
-  //   }
-  // }
+    }
+  }
 
 
-  // drawTextContainer(productTitleFieldText,productDescFieldText);
+  drawTextContainer(productTitleFieldText,productDescFieldText);
 }
 
 
